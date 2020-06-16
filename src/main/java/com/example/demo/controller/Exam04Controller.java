ca0b2dfcd7b7f485a7c86cde9bd38e518e8bb325
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 15:12:55 +0900

    演習4　トップ画面のコントローラーを実装

---
+++ src/main/java/com/example/demo/controller/Exam04Controller.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.form.UserForm;
+
+@Controller
+@RequestMapping("/ex04")
+public class Exam04Controller {
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+}
