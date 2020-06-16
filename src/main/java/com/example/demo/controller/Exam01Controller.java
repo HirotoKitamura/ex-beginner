f091c258bc3d6f0adfc48f9b78ac7cc2e2f65696
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 14:10:13 +0900

    結果画面のコントローラーを実装

--- src/main/java/com/example/demo/controller/Exam01Controller.java
+++ src/main/java/com/example/demo/controller/Exam01Controller.java
@@ -4,11 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 演習1のコントローラー.
+ * @author hiroto.kitamura
+ *
+ */
 @Controller
 @RequestMapping("/ex01")
 public class Exam01Controller {
+	/**
+	 * トップ画面のコントローラー.
+	 * @return
+	 */
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
+	}
+	
+	/**
+	 * 結果画面のコントローラー.
+	 * @param name フォームから受け取った名前
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("result")
+	public String result(String name, Model model) {
+		model.addAttribute("name", name);
+		return "exam01-result";
 	}
 }
