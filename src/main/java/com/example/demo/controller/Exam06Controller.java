1d1e4750ad3942a7c28c29373a00afa8bb49ae93
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 17:29:19 +0900

    演習6　コントローラのdeleteメソッドを実装(仮)

--- src/main/java/com/example/demo/controller/Exam06Controller.java
+++ src/main/java/com/example/demo/controller/Exam06Controller.java
@@ -15,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.domain.Item;
 
 /**
- * 演習6　コントローラークラス
+ * 演習6 コントローラークラス
+ * 
  * @author hiroto.kitamura
  *
  */
@@ -24,10 +25,10 @@ import com.example.demo.domain.Item;
 public class Exam06Controller {
 	@Autowired
 	private ServletContext application;
-	
+
 	@Autowired
 	private HttpSession session;
-	
+
 	@RequestMapping("")
 	public String index(Model model) {
 		List<Item> itemList = new ArrayList<>();
@@ -36,27 +37,27 @@ public class Exam06Controller {
 		itemList.add(new Item("ファイル", 2000));
 		application.setAttribute("itemList", itemList);
 		int totalPrice = 0;
-		if(session.getAttribute("cart") != null) {
+		if (session.getAttribute("cart") != null) {
 			@SuppressWarnings("unchecked")
-			List<Item> cart = (List<Item>)session.getAttribute("cart");
-			for(Item item : cart) {
+			List<Item> cart = (List<Item>) session.getAttribute("cart");
+			for (Item item : cart) {
 				totalPrice += item.getPrice();
 			}
 		}
 		model.addAttribute("totalPrice", totalPrice);
 		return "item-and-cart";
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@RequestMapping("in-cart")
 	public String inCart(int numberToAdd, Model model) {
-		List<Item> itemList = (List<Item>)application.getAttribute("itemList");
+		List<Item> itemList = (List<Item>) application.getAttribute("itemList");
 		Item item = itemList.get(numberToAdd);
 		List<Item> cart;
-		if(session.getAttribute("cart") == null) {
+		if (session.getAttribute("cart") == null) {
 			cart = new LinkedList<>();
 		} else {
-			cart = (List<Item>)session.getAttribute("cart");
+			cart = (List<Item>) session.getAttribute("cart");
 		}
 		cart.add(item);
 		session.setAttribute("cart", cart);
@@ -65,7 +66,14 @@ public class Exam06Controller {
 
 	@RequestMapping("delete")
 	public String delete(int numberToDelete, Model model) {
+		if (session.getAttribute("cart") == null) {
+			return index(model);
+		}
+		@SuppressWarnings("unchecked")
+		List<Item> cart = (List<Item>) session.getAttribute("cart");
+		cart.remove(numberToDelete);
+		session.setAttribute("cart", cart);
 		return index(model);
 	}
-	
+
 }
