f873a1effb303424ce6db307bbcfcef34af8c8eb
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 14:20:41 +0900

    演習2　結果画面のコントローラーを実装

--- src/main/java/com/example/demo/controller/Exam02Controller.java
+++ src/main/java/com/example/demo/controller/Exam02Controller.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Exam02Controller {
 	@Autowired
 	private HttpSession session;
-	
+
 	@RequestMapping("")
 	public String index() {
 		return "exam02";
+	}
+
+	@RequestMapping("result")
+	public String result(int num1, int num2) {
+		session.setAttribute("result", num1 + num2);
+		return "exam02-result";
 	}
 }
