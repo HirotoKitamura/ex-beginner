0e6db983a1ed7bae683dfb5bf43706d50865c4fa
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-16 14:37:37 +0900

    演習3　結果画面のコントローラーを実装

--- src/main/java/com/example/demo/controller/Exam03Controller.java
+++ src/main/java/com/example/demo/controller/Exam03Controller.java
@@ -8,21 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 演習3のコントローラー.
+ * 
  * @author hiroto.kitamura
  *
  */
 @Controller
 @RequestMapping("/ex03")
 public class Exam03Controller {
-	@Autowired 
+	@Autowired
 	private ServletContext application;
-	
+
 	/**
 	 * トップ画面のコントローラー.
+	 * 
 	 * @return
 	 */
 	@RequestMapping("")
 	public String index() {
 		return "exam03";
+	}
+
+	@RequestMapping("result")
+	public String result(int num1, int num2, int num3) {
+		application.setAttribute("price", num1 + num2 + num3);
+		application.setAttribute("pricewithtax", (int) (num1 + num2 + num3) * 1.1);
+		return "exam03-result";
 	}
 }
